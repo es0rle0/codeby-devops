84e675cdfd757a31d9a7afa7e868aa7b87ecec88
es0rle0 <altonskaaekaterina@gmail.com>
2026-03-23 17:14:07 +0300

    Lesson26: fix hello-jenkins test after message change

--- lesson26/apps/hello-jenkins/src/test/java/ru/codeby/AppTest.java
+++ lesson26/apps/hello-jenkins/src/test/java/ru/codeby/AppTest.java
@@ -6,6 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class AppTest {
     @Test
     void messageIsCorrect() {
-        assertEquals("Hello Jenkins!", App.message());
+        assertEquals("Hello Jenkins!!!", App.message());
     }
 }
