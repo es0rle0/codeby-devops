acb26cc308879018cec8fd9c7f336e9e6277dd3c
es0rle0 <altonskaaekaterina@gmail.com>
2026-03-24 11:42:13 +0300

    Lesson27: add lesson26 apps for Sonar analysis

---
+++ lesson26/apps/hello-jenkins/src/test/java/ru/codeby/AppTest.java
@@ -0,0 +1,11 @@
+package ru.codeby;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    void messageIsCorrect() {
+        assertEquals("Hello Jenkins!!!", App.message());
+    }
+}
